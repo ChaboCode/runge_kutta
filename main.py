2c88b160c25ed9f8c76e8af5b30c6f26aa10be8e
ChaboCode <kaerdos.dev@protonmail.com>
2025-08-05 16:53:18 -0600

    Added polynomial approximation

--- main.py
+++ main.py
@@ -6,33 +6,52 @@ import matplotlib.pyplot as plt
 from math import *
 import io
 
-app  = FastAPI()
+app = FastAPI()
 
 app.add_middleware(
-  CORSMiddleware,
-  allow_origins=['*'],
-  allow_headers=['*'],
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_headers=["*"],
 )
+
 
 @app.get("/")
 def root_page():
     return {"Hola": "sexo"}
 
-@app.get("/grunt-kutta")
-def grunt_kutta_route(f_str: str, x0: float, y0: float, h: float, end: float):
-    f = eval(f'lambda x, y: {f_str}')
-    result, y = runge_kutta(f, x0, y0, h, end)
-    return {"result": result, "steps": y}
+
+@app.get("/values")
+def grunt_kutta_route(f_str: str, x0: float, y0: float, steps: int, end: float):
+    h = (end - x0) / steps
+    f = eval(f"lambda x, y: {f_str}")
+    result, y, k1, k2, k3, k4 = runge_kutta(f, x0, y0, h, end)
+    return {
+        "result": result,
+        "steps": y,
+        "k1": k1,
+        "k2": k2,
+        "k3": k3,
+        "k4": k4,
+    }
+
 
 @app.get("/img")
-def runge_kutta_pic(f_str: str, x0: float, y0: float, h: float, end: float):
-    f = eval(f'lambda x, y: {f_str}')
-    result, y = runge_kutta(f, x0, y0, h, end)
-    x = np.arange(x0, end +h, h)
+def runge_kutta_pic(f_str: str, x0: float, y0: float, steps: int, end: float):
+
+    h = (end - x0) / steps
+    f = eval(f"lambda x, y: {f_str}")
+
+    result, y, k1, k2, k3, k4 = runge_kutta(f, x0, y0, h, end)
+    x = np.arange(x0, end + h, h)
 
     fig, ax = plt.subplots()
-    # ax.set_title("Te quedas pen de jo")
-    ax.plot(x, y)
+    ax.plot(x, y, "o")
+
+    f_poly, x_poly, y_poly = approximate(x, y)
+    print(f_poly)
+    ax.plot(x_poly, y_poly, "-")
+    ax.grid()
+
     buf = io.BytesIO()
     fig.savefig(buf, format="png")
     plt.close(fig)
@@ -42,24 +61,40 @@ def runge_kutta_pic(f_str: str, x0: float, y0: float, h: float, end: float):
 
 
 def runge_kutta(f, x0, y0, h, end):
-  y = [y0]
-  yn = y0
-  for i in np.arange(x0, end, h):
-    xn = i
-    k1 = f(xn, yn)
-    # print('k1: ', k1)
+    y = [y0]
+    yn = y0
+    k1s = []
+    k2s = []
+    k3s = []
+    k4s = []
+    for i in np.arange(x0, end, h):
+        xn = i
+        k1 = f(xn, yn)
+        k1s.append(k1)
+        # print('k1: ', k1)
 
-    k2 = f(xn + h/2, yn + (h/2) * k1)
-    # print('k2: ', k2)
+        k2 = f(xn + h / 2, yn + (h / 2) * k1)
+        k2s.append(k2)
+        # print('k2: ', k2)
 
-    k3 = f(xn + h/2, yn + (h/2) * k2)
-    # print('k3: ', k3)
+        k3 = f(xn + h / 2, yn + (h / 2) * k2)
+        k3s.append(k3)
+        # print('k3: ', k3)
 
-    k4 = f(xn + h, yn + h * k3)
-    # print('k4: ', k4)
+        k4 = f(xn + h, yn + h * k3)
+        k4s.append(k4)
+        # print('k4: ', k4)
 
-    yn = yn + (h/6) *(k1 + 2 * k2 + 2 * k3 + k4)
-    y.append(yn)
-    # print('yn: ', yn)
-  return yn, y
+        yn = yn + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
+        y.append(yn)
+        # print('yn: ', yn)
+    return yn, y, k1s, k2s, k3s, k4s
 
+
+def approximate(x, y):
+    coef = np.polyfit(x, y, deg=3)
+    f_poly = np.poly1d(coef)
+    x_poly = np.linspace(min(x), max(x), 100)
+    y_poly = f_poly(x_poly)
+
+    return f_poly, x_poly, y_poly
